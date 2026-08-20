833687c850cc39a1a358788930bd7b30e9b03c15
Joan Mz Linux <hola@112books.eu>
2026-08-20 17:43:43 +0200

    fix: add CSRF_TRUSTED_ORIGINS for cms.naubostik.com

--- web-cms/webcms/settings.py
+++ web-cms/webcms/settings.py
@@ -12,6 +12,7 @@ if env_path.exists():
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-key-change-me")
 DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = ["https://cms.naubostik.com"]
 
 INSTALLED_APPS = [
     "home",
